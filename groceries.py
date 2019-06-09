f267b884aebac9f8e990abe462bd31e3d24063d1
Rob Sansone <50809985+RNS341@users.noreply.github.com>
2019-06-09 18:17:48 -0400

    Sorted list

--- groceries.py
+++ groceries.py
@@ -29,7 +29,12 @@ print("--------------")
 print("There are " + str(products_count) + " products:")
 print("--------------")
 
-for p in products:
+def sort_by_name(any_product):
+    return any_product["name"]
+
+sorted_products = sorted(products, key=sort_by_name)
+
+for p in sorted_products:
     price_usd = "(${0:.2f})".format(p["price"])
     print(" + " + p["name"] +" "+ price_usd)
    
